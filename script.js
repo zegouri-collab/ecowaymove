c39fe5f6965e9c7f87b0a78cc1742b7045f38f45
zegouri-collab <zegouri@gmail.com>
2025-11-23 14:05:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function showContactMessage(elementId) {
   msg.innerHTML =
     "<strong>Merci pour votre intérêt !</strong><br>" +
     "Pour une réponse rapide, contactez-nous au <a href='tel:0631639079'>06 31 63 90 79</a><br>" +
-    "ou visitez <a href='Mail: ecowaytec@gmail.com' target='_blank'>ecowaytec@gmail.com</a>";
+    "ou par mail : <a href='mailto:ecowaytec@gmail.com'>ecowaytec@gmail.com</a>";
   
   msg.style.display = "block";
   
@@ -97,6 +97,3 @@ backToTopBtn.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
-
-
-
